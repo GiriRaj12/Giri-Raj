ed559e5de907e56bb6d8dfca7a13f17be078dc69
Giri Raj <40237937+GiriRaj12@users.noreply.github.com>
2018-10-17 14:07:53 +0530

    Create first.java

---
+++ first.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.lang.annotation.*;
+public class Project {
+    public static void main(String []args) {
+        Scanner in=new Scanner(System.in);
+        int n=in.nextInt();
+        int a[]=new int[n];
+        for(int i=0;i<n;i++){
+            a[i]=in.nextInt();
+        }
+        int count=0;
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                if(a[i]==a[j]){
+                    System.out.print(a[i]);
+                    count++;
+                    break;
+                }
+            }
+            if(count==1){
+                break;
+            }
+        }
+   }
+
+}
